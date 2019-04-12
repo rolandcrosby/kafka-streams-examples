25478dd1078ff9eb79a1a64401b3b295b8a3c8d2
Roland Crosby <roland@cockroachlabs.com>
2019-04-12 15:36:48 -0400

    i don't know how avro works

--- src/main/java/com/cockroachlabs/ClusterLogicalTimestampExtractor.java
+++ src/main/java/com/cockroachlabs/ClusterLogicalTimestampExtractor.java
@@ -1,22 +1,23 @@
 package com.cockroachlabs;
 
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.util.Utf8;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class ClusterLogicalTimestampExtractor implements org.apache.kafka.streams.processor.TimestampExtractor {
     @Override
     public long extract(final ConsumerRecord<Object, Object> consumerRecord, final long l) {
         final GenericRecord value = (GenericRecord) consumerRecord.value();
-        String ts;
-        ts = (String) value.get("resolved");
+        Utf8 ts;
+        ts = (Utf8) value.get("resolved");
         if (ts == null) {
-            ts = (String) value.get("updated");
+            ts = (Utf8) value.get("updated");
             if (ts == null) {
                 throw new RuntimeException("can't find resolved or updated timestamp");
             }
         }
-
-        final int decimal = ts.indexOf(".");
-        return Integer.parseInt(ts.substring(0, decimal - 6));
+        final String tsString = ts.toString();
+        final int decimal = tsString.indexOf(".");
+        return Integer.parseInt(tsString.substring(0, decimal - 6));
     }
 }
