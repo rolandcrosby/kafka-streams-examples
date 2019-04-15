aa35d0eddca41a3aa7c323a43a2df0ea2ef89106
Roland Crosby <roland@cockroachlabs.com>
2019-04-15 16:42:02 -0400

    SO-inspired refactoring and messing with serde

--- src/main/java/com/cockroachlabs/CRDBCDCConsumer.java
+++ src/main/java/com/cockroachlabs/CRDBCDCConsumer.java
@@ -3,16 +3,13 @@ package com.cockroachlabs;
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.util.Utf8;
+import org.apache.commons.collections.map.SingletonMap;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.ValueTransformerWithKey;
-import org.apache.kafka.streams.kstream.ValueTransformerWithKeySupplier;
-import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.state.StoreBuilder;
 import org.apache.kafka.streams.state.Stores;
@@ -33,54 +30,22 @@ public class CRDBCDCConsumer {
         streamsConfiguration.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
         streamsConfiguration.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
 
+        final String stateStoreName = "latest-timestamp-store";
+
         final StreamsBuilder builder = new StreamsBuilder();
         final KStream<GenericRecord, GenericRecord> inStream = builder.stream(topic);
 
-        final Serde<GenericRecord> stateSerde = new GenericAvroSerde();
+        inStream.transformValues(() -> new CRDBExactlyOnceTransformer(stateStoreName), stateStoreName);
+
+        final Serde<GenericRecord> stateKeySerde = new GenericAvroSerde();
+        stateKeySerde.configure(new SingletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081"), true);
         final StoreBuilder<KeyValueStore<GenericRecord, String>> tsStoreBuilder = Stores.keyValueStoreBuilder(
                 Stores.persistentKeyValueStore("latest-timestamp-store"),
-                stateSerde,
+                stateKeySerde,
                 Serdes.String()
         );
         builder.addStateStore(tsStoreBuilder);
 
-        inStream.transformValues(new ValueTransformerWithKeySupplier<GenericRecord, GenericRecord, GenericRecord>() {
-            @Override
-            public ValueTransformerWithKey<GenericRecord, GenericRecord, GenericRecord> get() {
-                return new ValueTransformerWithKey<GenericRecord, GenericRecord, GenericRecord>() {
-                    private KeyValueStore<GenericRecord, String> state;
-
-                    @Override
-                    public void init(final ProcessorContext processorContext) {
-                        this.state = (KeyValueStore<GenericRecord, String>) processorContext.getStateStore("latest-timestamp-store");
-                    }
-
-                    @Override
-                    public GenericRecord transform(final GenericRecord key, final GenericRecord value) {
-                        Utf8 ts = (Utf8) value.get("updated");
-                        if (ts == null) {
-                            ts = (Utf8) value.get("resolved");
-                            if (ts == null) {
-                                throw new RuntimeException("timestamp not found in input message");
-                            }
-                        }
-                        final String tsString = ts.toString();
-                        final String lastTS = this.state.get(key);
-                        if (lastTS == null || tsString.compareTo(lastTS) > 0) {
-                            this.state.put(key, tsString);
-                            return value;
-                        }
-                        return null;
-                    }
-
-                    @Override
-                    public void close() {
-
-                    }
-                };
-            }
-        }, "latest-timestamp-store")
-                .to("polished_" + topic);
 
         final KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
         streams.cleanUp();
---
+++ src/main/java/com/cockroachlabs/CRDBExactlyOnceTransformer.java
@@ -0,0 +1,47 @@
+package com.cockroachlabs;
+
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.util.Utf8;
+import org.apache.kafka.streams.kstream.ValueTransformerWithKey;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+public class CRDBExactlyOnceTransformer implements ValueTransformerWithKey<GenericRecord, GenericRecord, GenericRecord> {
+    private ProcessorContext processorContext;
+    private String stateStoreName;
+    private KeyValueStore<GenericRecord, String> state;
+
+    public CRDBExactlyOnceTransformer(final String stateStoreName) {
+        this.stateStoreName = stateStoreName;
+    }
+
+    @Override
+    public void init(final ProcessorContext processorContext) {
+        this.processorContext = processorContext;
+        this.state = (KeyValueStore) this.processorContext.getStateStore(stateStoreName);
+    }
+
+    @Override
+    public GenericRecord transform(final GenericRecord key, final GenericRecord value) {
+        Utf8 ts = (Utf8) value.get("updated");
+        if (ts == null) {
+            ts = (Utf8) value.get("resolved");
+            if (ts == null) {
+                throw new RuntimeException("timestamp not found in input message");
+            }
+        }
+        final String tsString = ts.toString();
+        final String lastTS = this.state.get(key);
+        if (lastTS == null || tsString.compareTo(lastTS) > 0) {
+            this.state.put(key, tsString);
+            return value;
+        }
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
+
